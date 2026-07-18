0637937aa8b5e738d2d4685a42f2fde5b75e4403
POS1 <POS1@CSS.local>
2026-07-18 10:21:53 -0400

    fix formidable import

--- api/files.js
+++ api/files.js
@@ -1,5 +1,5 @@
 const { put, del } = require("@vercel/blob");
-const formidable = require("formidable");
+const { formidable } = require("formidable");
 const fs = require("node:fs/promises");
 
 const { getDb, toObjectId, serialize } = require("../lib/db");
